69c35c7213772d69f5be9437f3cf1d0dafaa6dd3
Diabolos2943 <cks121120@gmail.com>
2015-01-06 15:40:59 +0900

    aaaaaa

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ORA");
-
         }
     }
 }
