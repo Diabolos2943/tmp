216a56f7a1ac2a588f5741090cc0b5c03ea6c9cc
Diabolos2943 <cks121120@gmail.com>
2015-01-06 15:39:05 +0900

    aaaaaaa

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("ORA");
-
+            https://github.com/Diabolos2943/tmp.git
         }
     }
 }
